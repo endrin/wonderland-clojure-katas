382424025f353d7b950a4c10c1936a5ef4c7c24e
endrin @ work <yon.drin@gmail.com>
2018-04-28 19:05:45 +0300

    Define non-public functions with defn-

--- alphabet-cipher/src/alphabet_cipher/coder.clj
+++ alphabet-cipher/src/alphabet_cipher/coder.clj
@@ -1,7 +1,7 @@
 (ns alphabet-cipher.coder
   (:require [clojure.string :as str]))
 
-(defn clamp-pos
+(defn- clamp-pos
   "Moves given `pos` to a 0-26 range"
   [pos]
   (loop [p pos]
@@ -9,47 +9,47 @@
       (mod p 26)
       (recur (+ p 26)))))
 
-(defn char->pos
+(defn- char->pos
   "Given a character `chr`, returns its offset relative to \\a"
   [^Character chr]
   (- (int chr) (int \a)))
 
-(defn pos->char
+(defn- pos->char
   "Given some offset `pos` relative to \\a turns it into actual char"
   [^Integer pos]
   (char (+ (clamp-pos pos) (int \a))))
 
-(defn letter+
+(defn- letter+
   "Sums its args thinking they are English alphabet letters"
   [& args]
   (pos->char (apply + (map char->pos args))))
 
-(defn letter-
+(defn- letter-
   "Subtracts its args thinking they are English alphabet letters"
   [& args]
   (pos->char (apply - (map char->pos args))))
 
-(defn ^Character code-table-cell
+(defn- ^Character code-table-cell
   "Finds a letter in a cell given a pair of row and column chars"
   [^Character row-char ^Character col-char]
   (letter+ row-char col-char))
 
-(defn ^Character code-table-row
+(defn- ^Character code-table-row
   "Finds a letter in a row given a pair of cell and column chars"
   [^Character cell-char ^Character col-char]
   (letter- cell-char col-char))
 
-(defn ^Character code-table-column
+(defn- ^Character code-table-column
   "Finds a letter in a column given a pair of cell and row chars"
   [^Character cell-char ^Character row-char]
   (letter- cell-char row-char))
 
-(defn rotating
+(defn- rotating
   "Rotates chars of the keyword `kw` infinitely"
   [kw]
   (cycle (lazy-seq kw)))
 
-(defn kw-boundary?
+(defn- kw-boundary?
   "If given `coll` consists of rotating fragments of length `n`,
    returns that `n`"
   [coll n]
@@ -59,7 +59,7 @@
            (str/starts-with? (first chunks) (last chunks)))
       n)))
 
-(defn not-rotating
+(defn- not-rotating
   "Given a rotating keyword `kw` converts it back to normal word"
   [kw]
   (let [possible-positions (range 1 (quot (count kw) 2))
