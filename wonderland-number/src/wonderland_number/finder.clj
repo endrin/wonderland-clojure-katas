6c434336815e5c16d07c5048e2db525b87f3b51e
endrin@home <yon.drin@gmail.com>
2018-05-03 15:46:24 +0300

    wonderland-number: abstract the solution, implement search for self-sum of cubes

--- wonderland-number/src/wonderland_number/finder.clj
+++ wonderland-number/src/wonderland_number/finder.clj
@@ -5,13 +5,22 @@
            (when (pos? n)
              (cons (mod n 10) (digit-iter (quot n 10))))))
         (digits [n] (sort (digit-iter n)))]
-  (defn wondernum? [n]
-    (when
-     (every? (partial = (digits n))
-             (->> (range 2 7)
-                  (map (partial * n))
-                  (map digits)))
-      n)))
+  (defn- wondernum? [n]
+    (every? (partial = (digits n))
+            (->> (range 2 7)
+                 (map (partial * n))
+                 (map digits))))
+  (defn- sum-of-cubes? [n]
+    (= n (apply + (map #(* % % %) (digits n))))))
+
+(defn- match-number [criterion]
+  #(when (criterion %) %))
+
+(defn- special-number [criterion range]
+  (some (match-number criterion) range))
 
 (defn wonderland-number []
-  (some wondernum? (range 100000 1000000)))
+  (special-number wondernum? (range 100000 1000000)))
+
+(defn self-cube-number []
+  (special-number sum-of-cubes? (range 100 1000)))
--- wonderland-number/test/wonderland_number/finder_test.clj
+++ wonderland-number/test/wonderland_number/finder_test.clj
@@ -7,6 +7,11 @@
         s2 (set (str n2))]
     (= s1 s2)))
 
+(defn sum-of-cubed-digits [n]
+  (let [digits (map (comp - (partial - (int \0)) int) (seq (str n)))
+        cubes (map #(* % % %) digits)]
+    (apply + cubes)))
+
 (deftest test-wonderland-number
   (testing "A wonderland number must have the following things true about it"
     (let [wondernum (wonderland-number)]
@@ -16,3 +21,8 @@
       (is (hasAllTheSameDigits? wondernum (* 4 wondernum)))
       (is (hasAllTheSameDigits? wondernum (* 5 wondernum)))
       (is (hasAllTheSameDigits? wondernum (* 6 wondernum))))))
+
+(deftest sum-of-cubes
+  (testing "A certain number must be equal to sum of its cubed digits"
+    (let [num-under-test (self-cube-number)]
+      (is (= num-under-test (sum-of-cubed-digits num-under-test))))))
