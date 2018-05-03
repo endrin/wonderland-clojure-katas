26c7c360b677b7b89cba461ed422ee22387b7f7e
endrin@home <yon.drin@gmail.com>
2018-05-03 03:49:30 +0300

    wonderland-number: implement solution

--- wonderland-number/src/wonderland_number/finder.clj
+++ wonderland-number/src/wonderland_number/finder.clj
@@ -1,5 +1,17 @@
 (ns wonderland-number.finder)
 
+(letfn [(digit-iter [n]
+          (lazy-seq
+           (when (pos? n)
+             (cons (mod n 10) (digit-iter (quot n 10))))))
+        (digits [n] (sort (digit-iter n)))]
+  (defn wondernum? [n]
+    (when
+     (every? (partial = (digits n))
+             (->> (range 2 7)
+                  (map (partial * n))
+                  (map digits)))
+      n)))
+
 (defn wonderland-number []
-  ;; calculate me
-  42)
+  (some wondernum? (range 100000 1000000)))
